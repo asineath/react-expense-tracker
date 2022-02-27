1289c549430406de3605188411146c5452a5ca2e
Alex Sineath <alex.sineath@yahoo.com>
2022-02-27 13:16:12 -0500

    Initial push to main

---
+++ react-expense-tracker/src/components/Chart/Chart.js
@@ -0,0 +1,23 @@
+
+import ChartBar from './ChartBar';
+import "./Chart.css";
+
+const Chart = (props) => {
+
+  const totalMaximum = Math.max(...props.dataPoints.map(o => o.value));
+
+  return (
+    <div className="chart">
+      {props.dataPoints.map((dataPoint) => (
+        <ChartBar
+          key={dataPoint.label}
+          value={dataPoint.value}
+          maxValue={totalMaximum}
+          label={dataPoint.label}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Chart;
